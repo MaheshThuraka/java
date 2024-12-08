bc8140dade8c8265e6490eef105b1ab551a57e3a
MaheshThuraka <maheshthuraka6363@gmail.com>
2024-12-08 20:28:13 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Area{
+    public static void main(String[] args) {
+        int r;
+        System.out.println("Enter the r value: ");
+        Scanner in=new Scanner(System.in);
+        r=in.nextInt();
+        System.out.println(3.14*r*r);
+    }
+}
